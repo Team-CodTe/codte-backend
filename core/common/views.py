6dab2e886921698951a5df44d8788454c18c13e2
Dongwoo Kang <alsdn1360@ajou.ac.kr>
2026-01-25 21:03:23 +0900

    Chore: 헬스 체크 API에 데코레이터 추가

--- core/common/views.py
+++ core/common/views.py
@@ -1,5 +1,7 @@
-from django.http import HttpResponse
+from django.http import JsonResponse
+from django.views.decorators.http import require_GET
 
 
+@require_GET
 def health_check(request):
-    return HttpResponse("OK")
+    return JsonResponse({"status": "ok"})
