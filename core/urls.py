3993885369eac60d17b18864717bd6eb231e516d
강동우 <110466553+alsdn1360@users.noreply.github.com>
2026-01-25 22:12:25 +0900

    Feat: UptimeRobot을 위한 간단한 헬스 체크 뷰 추가 (#42)
    * Feat: UptimeRobot을 위한 간단한 헬스 체크 뷰 추가
    
    - Render 무료 티어는 15분마다 서버가 잠자기 모드가 되는데, 이걸 방지하기 위해 5분마다 찌르기 위해 UptimeRobot을 도입함
    - 로그아웃에 쿠키 삭제 함수 적용
    - 배포 환경에서 samesite 옵션 Lax 적용
    
    * Chore: 헬스 체크 URL 마지막에 슬래시추가
    
    * Fix: 쿠키 samesite 옵션 통일
    
    * Chore: 헬스 체크 API에 데코레이터 추가
    
    * Fix: health-check url 최상단 배치
    
    * Chore: health-check api 데코레이터 수정

--- core/urls.py
+++ core/urls.py
@@ -1,6 +1,7 @@
 from django.urls import include, path
 
 urlpatterns = [
+    path("", include("core.common.urls")),
     path("", include("core.auth.urls")),
     path("", include("core.user.urls")),
     path("", include("core.study.urls")),
